be00cc351bf335f8f0c2fe8612e26d4fc59702ad
Your Name <you@example.com>
2025-12-25 22:01:00 +0530

    Initial commit

--- STRIVER SHEET/sortting ALgos/insertionsort.cpp
+++ STRIVER SHEET/sortting ALgos/insertionsort.cpp
@@ -8,9 +8,9 @@ int arr[10]= {9,13,45,78,20,25,46,48,99,100};
 int n = 10;
 
 
-for(int i = 0; i<=n-1; i++){
+for(int i = 1; i<=n-1; i++){
     int j = i;
-    while(j > 0 && arr[j-1]> arr[j]){
+    while(j >= 0 && arr[j-1]> arr[j]){
         swap(arr[j-1],arr[j]);
         j--;
     }
